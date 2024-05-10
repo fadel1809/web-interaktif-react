e67ffe95f8148f24451f8776343c8adc566cb06e
Fadel <fadelmaulana12@gmail.com>
2024-05-10 11:42:29 +0700

    initial-final

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,10 @@
+import Wrapper from "../assets/wrapper/landingPage"
+
 const LandingPage = () => {
   return (
+    <Wrapper>
     <div>LandingPage</div>
+    </Wrapper>
   )
 }
 export default LandingPage
